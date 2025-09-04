fd7419e1c968a51622ea33d29ab7f5344a6140b8
Bala Priya C <47279635+balapriyac@users.noreply.github.com>
2025-09-04 08:51:21 +0530

    Update distances.py

--- distance-metrics/distances.py
+++ distance-metrics/distances.py
@@ -1,1 +1,20 @@
 # INSTALL SciPy
+from scipy.spatial import distance
+
+x = [3,6,9]
+y = [1,0,1]
+
+print(distance.euclidean(x,y))
+# Output >> 10.198039027185569
+
+print(distance.cityblock(x,y))
+# Output >> 16
+
+print(distance.minkowski(x,y,p=3))
+# Output >> 9.028714870948003
+
+print(distance.minkowski(x,y,p=1))
+# Output >> 16.0
+
+print(distance.minkowski(x,y,p=2))
+# Output >> 10.198039027185569
