2b32a8164005637ea3ad1a9a92aa004ae1ef9b04
Bala Priya C <47279635+balapriyac@users.noreply.github.com>
2025-09-05 10:18:38 +0530

    Update calculate-norms.py

--- numpy-linalg-norm/calculate-norms.py
+++ numpy-linalg-norm/calculate-norms.py
@@ -8,3 +8,17 @@ from numpy import linalg
 
 l2_norm = linalg.norm(vector)
 print(f"{l2_norm = :.2f}")
+
+# Output >> l2_norm = 9.54
+
+l2_norm = linalg.norm(vector, ord=2)
+print(f"{l2_norm = :.2f}")
+
+# Output >> l2_norm = 9.54
+
+l1_norm = linalg.norm(vector, ord=1)
+print(f"{l1_norm = :.2f}")
+
+# Output >> l1_norm = 21.00
+
+
