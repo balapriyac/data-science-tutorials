b2dc999ad915d4a0deb036fe9a9936a40acef1d6
Bala Priya C <47279635+balapriyac@users.noreply.github.com>
2025-09-08 13:59:09 +0530

    Update pca-with-sklearn.py

--- pca-sklearn/pca-with-sklearn.py
+++ pca-sklearn/pca-with-sklearn.py
@@ -1,1 +1,35 @@
+from sklearn import datasets
+wine_data = datasets.load_wine(as_frame=True)
+df = wine_data.data
 
+print(df.shape)
+
+from sklearn.preprocessing import StandardScaler
+std_scaler = StandardScaler()
+scaled_df = std_scaler.fit_transform(df)
+
+from sklearn.decomposition import PCA
+pca = PCA(n_components=3)
+pca.fit_transform(scaled_df)
+
+pca.components_
+
+sum(pca.explained_variance_ratio_)
+
+import numpy as np
+nums = np.arange(14)
+
+var_ratio = []
+for num in nums:
+  pca = PCA(n_components=num)
+  pca.fit(scaled_df)
+  var_ratio.append(np.sum(pca.explained_variance_ratio_))
+
+import matplotlib.pyplot as plt
+
+plt.figure(figsize=(4,3),dpi=150)
+plt.grid()
+plt.plot(nums,var_ratio,marker='o')
+plt.xlabel('n_components')
+plt.ylabel('Explained variance ratio')
+plt.title('n_components vs. Explained Variance Ratio')
